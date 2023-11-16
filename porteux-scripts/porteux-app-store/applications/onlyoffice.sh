446000d67d430ba80ca1c19b41a4647d3365288e
porteux <porteuxlinux@gmail.com>
2023-11-16 10:48:03 +0100

    Changed OnlyOffice repository to AppImage

--- porteux-scripts/porteux-app-store/applications/onlyoffice.sh
+++ porteux-scripts/porteux-app-store/applications/onlyoffice.sh
@@ -3,9 +3,9 @@
 CURRENTPACKAGE=onlyoffice
 FRIENDLYNAME="OnlyOffice - Desktop Editors"
 CATEGORY=Office
-FULLVERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/ONLYOFFICE/DesktopEditors/releases/latest | rev | cut -d / -f 1 | rev)
+FULLVERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/ONLYOFFICE/appimage-desktopeditors/releases/latest | rev | cut -d / -f 1 | rev)
 VERSION="${FULLVERSION//[vV]}"
-APPLICATIONURL="https://github.com/ONLYOFFICE/DesktopEditors/releases/latest/download/DesktopEditors-x86_64.AppImage"
+APPLICATIONURL="https://github.com/ONLYOFFICE/appimage-desktopeditors/releases/latest/download/DesktopEditors-x86_64.AppImage"
 ACTIVATEMODULE=$([[ "$@" == *"--activate-module"* ]] && echo "--activate-module")
 
 RESULT=$(/opt/porteux-scripts/porteux-app-store/appimage-builder.sh "$CURRENTPACKAGE" "$FRIENDLYNAME" "$CATEGORY" "$APPLICATIONURL" "$VERSION" "$ACTIVATEMODULE")
