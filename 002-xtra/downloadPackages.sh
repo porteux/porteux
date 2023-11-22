e138e0650fc3e7d3b42aee4da0e21080ec64e6c7
porteux <porteuxlinux@gmail.com>
2023-11-22 12:01:02 +0100

    Added support to new mpv 0.37.0

--- 002-xtra/downloadPackages.sh
+++ 002-xtra/downloadPackages.sh
@@ -14,6 +14,10 @@ wait
 
 DownloadPackage "frei0r-plugins" & # temporary to build ffmpeg
 DownloadPackage "opencl-headers" & # temporary to build ffmpeg
+DownloadPackage "python-glad2" & # temporary to build libplacebo
+DownloadPackage "python-Jinja2" & # temporary to build libplacebo
+DownloadPackage "python-MarkupSafe" & # temporary to build libplacebo
+DownloadPackage "vulkan-sdk" & # temporary to build libplacebo
 wait
 
 ### script clean up
