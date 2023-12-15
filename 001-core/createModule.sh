693383258b230366b4b6ca7b255dd9127c4fe1ca
porteux <porteuxlinux@gmail.com>
2023-12-15 13:58:21 +0100

    .Changed hostname to porteux .Added liburing in 001-core .Added pipewire in 002-xorg .Removed pipewire from KDE and GNOME .Removed watch-xorg script -- it keeps running in the background, doesn't work in Wayland, etc .In 001-core build script make sure we always get latest procps package

--- 001-core/createModule.sh
+++ 001-core/createModule.sh
@@ -114,7 +114,7 @@ installpkg $MODULEPATH/packages/ncurses*.txz || exit 1
 currentPackage=procps
 mkdir $MODULEPATH/${currentPackage} && cd $MODULEPATH/${currentPackage}
 cp $SCRIPTPATH/extras/procps/* .
-version=$(curl -s https://gitlab.com/${currentPackage}-ng/${currentPackage}/-/tags?format=atom | grep ' <title>' | grep -v rc | head -1 | cut -d '>' -f 2 | cut -d '<' -f 1)
+version=$(curl -s https://gitlab.com/${currentPackage}-ng/${currentPackage}/-/tags?format=atom | grep ' <title>' | grep -v rc | sort -V -r | head -1 | cut -d '>' -f 2 | cut -d '<' -f 1)
 sed -i "s|VERSION=\${VERSION.*|VERSION=\${VERSION:-${version//[vV]}}|g" ${currentPackage}.SlackBuild
 wget https://gitlab.com/${currentPackage}-ng/procps/-/archive/${version}/procps-${version}.tar.gz
 sh ${currentPackage}.SlackBuild || exit 1
