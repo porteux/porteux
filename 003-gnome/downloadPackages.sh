a0d4fbacab42ae76d346573ce4edfb4b6bd0feff
porteux <porteuxlinux@gmail.com>
2024-01-12 20:16:59 +0100

    Remaining files

---
+++ 003-gnome/downloadPackages.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+source "$PWD/../builder-utils/slackwarerepository.sh"
+
+REPOSITORY="$1"
+
+GenerateRepositoryUrls "$REPOSITORY"
+
+DownloadPackage "accountsservice" &
+DownloadPackage "aspell" &
+DownloadPackage "dconf" &
+DownloadPackage "editorconfig-core-c" &
+DownloadPackage "enchant" &
+DownloadPackage "ffmpegthumbnailer" &
+DownloadPackage "gexiv2" &
+wait
+DownloadPackage "gjs" &
+DownloadPackage "hunspell" &
+DownloadPackage "glib-networking" &
+DownloadPackage "gperf" &
+DownloadPackage "gst-plugins-bad-free" &
+DownloadPackage "gst-plugins-good" &
+DownloadPackage "gst-plugins-libav" &
+wait
+DownloadPackage "gtk4" &
+DownloadPackage "hyphen" &
+DownloadPackage "ibus" &
+DownloadPackage "libcanberra" &
+DownloadPackage "libgtop" &
+DownloadPackage "libhandy" &
+wait
+DownloadPackage "libnma" &
+DownloadPackage "libproxy" &
+DownloadPackage "libxklavier" &
+DownloadPackage "libyaml" &
+DownloadPackage "woff2" &
+DownloadPackage "xorg-server-xwayland" &
+wait
+
+### packages that require specific striping
+
+DownloadPackage "gtk+3" &
+wait
+
+### temporary packages for further building
+
+DownloadPackage "boost" &
+DownloadPackage "cups" &
+DownloadPackage "dbus-python" &
+DownloadPackage "egl-wayland" &
+DownloadPackage "iso-codes" &
+DownloadPackage "krb5" &
+wait
+DownloadPackage "libsass" & # required by gnome-console
+DownloadPackage "libsoup3" &
+DownloadPackage "libwnck3" &
+DownloadPackage "python-pip" &
+DownloadPackage "sassc" & # required by gnome-console
+DownloadPackage "xtrans" &
+wait
+
+### script clean up
+
+rm FILE_LIST
+rm serverPackages.txt
