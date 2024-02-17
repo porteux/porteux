e823df92fa6c4092819c96ce2cf1dc0ea422a4fe
porteux <porteuxlinux@gmail.com>
2024-02-17 12:53:20 +0100

    .Moved expat from devel to xorg .Reverted AgressiveStripAll in xorg and xtra .Refactored strip script to be more flexible and consistent

--- 0050-multilib-lite/createModule.sh
+++ 0050-multilib-lite/createModule.sh
@@ -149,16 +149,13 @@ find $MODULEPATH/packages/bin \( -type f -o -type l \) ! -name "sln" -delete
 find $MODULEPATH/packages/usr -mindepth 1 -maxdepth 1 -type d ! -name "lib" -exec rm -rf {} +
 find $MODULEPATH/packages/usr/lib/locale -mindepth 1 -maxdepth 1 -type d ! -name "en_US.utf8" -exec rm -rf {} +
 
-mv $MODULEPATH/packages/lib $MODULEPATH/ # move out /lib so we can strip safely
-mv $MODULEPATH/packages/usr/lib/dri $MODULEPATH/ # move out usr/lib/dri so we can strip safely
-mv $MODULEPATH/packages/usr/lib/libgcc* $MODULEPATH/
-mv $MODULEPATH/packages/usr/lib/libstdc++* $MODULEPATH/
+# move out things that don't support stripping
+mv $MODULEPATH/packages/lib $MODULEPATH/
+mv $MODULEPATH/packages/usr/lib/dri $MODULEPATH/
 GenericStrip
 AggressiveStripAll
 mv $MODULEPATH/lib $MODULEPATH/packages/
 mv $MODULEPATH/dri $MODULEPATH/packages/usr/lib/
-mv $MODULEPATH/libgcc* $MODULEPATH/packages/usr/lib/
-mv $MODULEPATH/libstdc++* $MODULEPATH/packages/usr/lib/
 
 ### finalize
 
--- builder-utils/genericstrip.sh
+++ builder-utils/genericstrip.sh
@@ -1,98 +1,84 @@
 #!/bin/sh
 
 GenericStrip() {
-	if [ "$1" ]; then
-		CURRENTDIR="$1"
-	else
-		CURRENTDIR="$PWD"
-	fi
+	rm -R etc/logrotate.d
+	rm -R usr/doc
+	rm -R usr/include
+	rm -R usr/man
+	rm -R usr/src
+	rm -R usr/info
+	rm -R usr/share/aclocal
+	rm -R usr/share/bash-completion
+	rm -R usr/share/cmake
+	rm -R usr/share/devhelp
+	rm -R usr/share/doc
+	rm -R usr/share/gdb
+	rm -R usr/share/gir-1.0
+	rm -R usr/share/gnome-control-center
+	rm -R usr/share/gtk-doc
+	rm -R usr/share/help
+	rm -R usr/share/icons/HighContrast
+	rm -R usr/share/icons/hicolor/64x64
+	rm -R usr/share/icons/hicolor/72x72
+	rm -R usr/share/icons/hicolor/96x96
+	rm -R usr/share/icons/hicolor/192x192
+	rm -R usr/share/icons/hicolor/256x256
+	rm -R usr/share/icons/hicolor/512x512
+	rm -R usr/share/locale
+	rm -R usr/share/man
+	rm -R usr/share/pkgconfig
+	rm -R usr/share/sounds
+	rm -R usr/share/themes/HighContrast
+	rm -R usr/share/vala
+	rm -R usr/share/*/translations
+	rm -R usr/lib/pkgconfig
+	rm -R usr/lib64/pkgconfig
+	rm -R usr/lib/cmake
+	rm -R usr/lib/systemd
+	rm -R usr/lib64/cmake
+	rm -R usr/lib64/gnome-keyring
+	rm -R usr/lib64/gtk-2.0/include
+	rm -R usr/lib64/python2.7
+	rm -R var/log/pkgtools
+	rm -R var/log/setup
+	rm -R var/lib/pkgtools/douninst.sh/
+	rm -R var/lib/pkgtools/setup
+	
+	rm usr/share/pixmaps/*.xpm
+	rm var/log/removed_packages
+	rm var/log/removed_scripts
+	rm var/log/removed_uninstall_scripts
 
-	rm -R "$CURRENTDIR"/etc/logrotate.d
-	rm -R "$CURRENTDIR"/usr/doc
-	rm -R "$CURRENTDIR"/usr/include
-	rm -R "$CURRENTDIR"/usr/man
-	rm -R "$CURRENTDIR"/usr/src
-	rm -R "$CURRENTDIR"/usr/info
-	rm -R "$CURRENTDIR"/usr/share/aclocal
-	rm -R "$CURRENTDIR"/usr/share/bash-completion
-	rm -R "$CURRENTDIR"/usr/share/cmake
-	rm -R "$CURRENTDIR"/usr/share/devhelp
-	rm -R "$CURRENTDIR"/usr/share/doc
-	rm -R "$CURRENTDIR"/usr/share/gdb
-	rm -R "$CURRENTDIR"/usr/share/gir-1.0
-	rm -R "$CURRENTDIR"/usr/share/gnome-control-center
-	rm -R "$CURRENTDIR"/usr/share/gtk-doc
-	rm -R "$CURRENTDIR"/usr/share/help
-	rm -R "$CURRENTDIR"/usr/share/icons/HighContrast
-	rm -R "$CURRENTDIR"/usr/share/icons/hicolor/64x64
-	rm -R "$CURRENTDIR"/usr/share/icons/hicolor/72x72
-	rm -R "$CURRENTDIR"/usr/share/icons/hicolor/96x96
-	rm -R "$CURRENTDIR"/usr/share/icons/hicolor/192x192
-	rm -R "$CURRENTDIR"/usr/share/icons/hicolor/256x256
-	rm -R "$CURRENTDIR"/usr/share/icons/hicolor/512x512
-	rm -R "$CURRENTDIR"/usr/share/locale
-	rm -R "$CURRENTDIR"/usr/share/man
-	rm -R "$CURRENTDIR"/usr/share/pkgconfig
-	rm -R "$CURRENTDIR"/usr/share/sounds
-	rm -R "$CURRENTDIR"/usr/share/themes/HighContrast
-	rm -R "$CURRENTDIR"/usr/share/vala
-	rm -R "$CURRENTDIR"/usr/share/*/translations
-	rm -R "$CURRENTDIR"/usr/lib/pkgconfig
-	rm -R "$CURRENTDIR"/usr/lib64/pkgconfig
-	rm -R "$CURRENTDIR"/usr/lib/cmake
-	rm -R "$CURRENTDIR"/usr/lib/systemd
-	rm -R "$CURRENTDIR"/usr/lib64/cmake
-	rm -R "$CURRENTDIR"/usr/lib64/gnome-keyring
-	rm -R "$CURRENTDIR"/usr/lib64/gtk-2.0/include
-	rm -R "$CURRENTDIR"/usr/lib64/python2.7
-	rm -R "$CURRENTDIR"/var/log/pkgtools
-	rm -R "$CURRENTDIR"/var/log/setup
-	rm -R "$CURRENTDIR"/var/lib/pkgtools/douninst.sh/
-	rm -R "$CURRENTDIR"/var/lib/pkgtools/setup
+	find . -name 'AUTHORS*' -delete
+	find . -name 'COPYING*' -delete
+	find . -name 'README*' -delete
+	find . -name 'LICENSE*' -delete
+	find . -name '*.a' -delete
+	find . -name '*.c' -delete
+	find . -name '*.cmake' -delete
+	find . -name '*.deps' -delete
+	find . -name '*.gir' -delete
+	find . -name '*.h' -delete
+	find . -name '*.la' -delete
+	find . -name '*.m4' -delete
+	find . -name '*.o' -delete
+	find . -name '*.pc' -delete
+	find . -name '*.prl' -delete
+	find . -name '*.vapi' -delete
 	
-	rm "$CURRENTDIR"/usr/share/pixmaps/*.xpm
-	rm "$CURRENTDIR"/var/log/removed_packages
-	rm "$CURRENTDIR"/var/log/removed_scripts
-	rm "$CURRENTDIR"/var/log/removed_uninstall_scripts
-
-	find "$CURRENTDIR" -name 'AUTHORS*' -delete
-	find "$CURRENTDIR" -name 'COPYING*' -delete
-	find "$CURRENTDIR" -name 'README*' -delete
-	find "$CURRENTDIR" -name 'LICENSE*' -delete
-	find "$CURRENTDIR" -name '*.a' -delete
-	find "$CURRENTDIR" -name '*.c' -delete
-	find "$CURRENTDIR" -name '*.cmake' -delete
-	find "$CURRENTDIR" -name '*.deps' -delete
-	find "$CURRENTDIR" -name '*.gir' -delete
-	find "$CURRENTDIR" -name '*.h' -delete
-	find "$CURRENTDIR" -name '*.la' -delete
-	find "$CURRENTDIR" -name '*.m4' -delete
-	find "$CURRENTDIR" -name '*.o' -delete
-	find "$CURRENTDIR" -name '*.pc' -delete
-	find "$CURRENTDIR" -name '*.prl' -delete
-	find "$CURRENTDIR" -name '*.vapi' -delete
+	find usr/share/mime/ -mindepth 1 -maxdepth 1 -not -name packages -exec rm -rf '{}' \; 2>/dev/null
 	
-	find "$CURRENTDIR"/usr/share/mime/ -mindepth 1 -maxdepth 1 -not -name packages -exec rm -rf '{}' \; 2>/dev/null
-	
-	find "$CURRENTDIR" | xargs file | egrep -e "executable|shared object" | grep ELF | cut -f 1 -d : | xargs strip -S --strip-unneeded -R .note.gnu.gold-version -R .comment -R .note -R .note.gnu.build-id -R .note.ABI-tag 2> /dev/null
+	find . | xargs file | egrep -e "executable|shared object" | grep ELF | cut -f 1 -d : | xargs strip -S --strip-unneeded -R .comment -R .jcr -R .note -R .note.ABI-tag -R .note.gnu.build-id -R .note.gnu.gold-version -R .note.GNU-stack 2> /dev/null
 }
 
 AggressiveStrip() {
-	if [ "$1" ]; then
-		CURRENTDIR="$1"
-	else
-		CURRENTDIR="$PWD"
-	fi
-
-	find "$CURRENTDIR" | xargs file | egrep -e "executable" | grep ELF | cut -f 1 -d : | xargs strip -S --strip-all -R .comment -R .eh_frame -R .eh_frame_hdr -R .eh_frame_ptr -R .jcr -R .note -R .note.ABI-tag -R .note.gnu.build-id -R .note.gnu.gold-version -R .note.GNU-stack 2> /dev/null
+	find . | xargs file | egrep -e "executable" | grep ELF | cut -f 1 -d : | xargs strip -S --strip-all -R .comment -R .eh_frame -R .eh_frame_hdr -R .eh_frame_ptr -R .jcr -R .note -R .note.ABI-tag -R .note.gnu.build-id -R .note.gnu.gold-version -R .note.GNU-stack 2> /dev/null
 }
 
 AggressiveStripAll() {
-	if [ "$1" ]; then
-		CURRENTDIR="$1"
-	else
-		CURRENTDIR="$PWD"
-	fi
-
-	find "$CURRENTDIR" | xargs file | egrep -e "executable|shared object" | grep ELF | cut -f 1 -d : | xargs strip -S --strip-all -R .comment -R .eh_frame -R .eh_frame_hdr -R .eh_frame_ptr -R .jcr -R .note -R .note.ABI-tag -R .note.gnu.build-id -R .note.gnu.gold-version -R .note.GNU-stack 2> /dev/null
+	find . | xargs file | egrep -e "executable|shared object" | grep ELF | cut -f 1 -d : | xargs strip -S --strip-all -R .comment -R .eh_frame -R .eh_frame_hdr -R .eh_frame_ptr -R .jcr -R .note -R .note.ABI-tag -R .note.gnu.build-id -R .note.gnu.gold-version -R .note.GNU-stack 2> /dev/null
 }
+
+if [ "$1" ]; then
+	"$1"
+fi
