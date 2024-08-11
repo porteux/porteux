480dc75b6c6f4c04678db3c2139c5887be459741
porteux <porteuxlinux@gmail.com>
2024-08-12 01:08:31 +0200

    .Added greetd to enable autologin .Added network-manager-applet -- cosmic network applet is still not working .Added xdg-desktop-portal-gtk to make cosmic-screen work -- still not 100%

--- 003-cosmic/downloadPackages.sh
+++ 003-cosmic/downloadPackages.sh
@@ -5,6 +5,19 @@ REPOSITORY="$1"
 
 GenerateRepositoryUrls "$REPOSITORY"
 
+DownloadPackage "libdbusmenu" &
+DownloadPackage "libnma" &
+DownloadPackage "network-manager-applet" &
+wait
+
+### slackware current only packages
+
+if [ $SLACKWAREVERSION == "current" ]; then
+	DownloadPackage "libappindicator" &
+	DownloadPackage "libindicator" &
+	wait
+fi
+
 ### temporary packages for further building
 
 DownloadPackage "llvm" & # required by cosmic-greeter
