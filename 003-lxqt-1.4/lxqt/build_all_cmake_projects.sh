f95e7b412e26760b79c536ed031278e45cc70c91
porteux <porteuxlinux@gmail.com>
2024-09-26 12:23:05 +0200

    Make sure only LXQt packages that support LTO will use this flag

--- 003-lxqt-1.4/lxqt/build_all_cmake_projects.sh
+++ 003-lxqt-1.4/lxqt/build_all_cmake_projects.sh
@@ -62,6 +62,8 @@ for d in $CMAKE_REPOS $OPTIONAL_CMAKE_REPOS
 do
 	echo $'\n'$'\n'"Building: $d using externally specified options: $ALL_CMAKE_FLAGS"$'\n'
 	mkdir -p "$MODULEPATH/lxqt/$d/build" && cd "$MODULEPATH/lxqt/$d/build" || exit 1
+	
+	[ "$d" != "lxqt-config" ] && [ "$d" != "lxqt-panel" ] && [ "$d" != "screengrab" ] && FLTO="-flto"
 
 	CXXFLAGS="$GCCFLAGS -Wp,-D_FORTIFY_SOURCE=2 -Wformat -Wformat-security -Wp,-D_REENTRANT -ftree-loop-distribute-patterns -Wl,-z -Wl,now -Wl,-z -Wl,relro -fno-semantic-interposition -fno-trapping-math -Wl,-sort-common -Wl,--enable-new-dtags -fvisibility-inlines-hidden -Wl,--enable-new-dtags -flto" cmake $ALL_CMAKE_FLAGS .. && "$CMAKE_MAKE_PROGRAM" -j$JOB_NUM || exit 1
 	version=`git describe | cut -d- -f1`
