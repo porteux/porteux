948884331b2ec9fe5317fb6fabbd6731558662ba
porteux <porteuxlinux@gmail.com>
2024-10-17 19:28:15 +0200

    Added kernel patch version to aufs download

--- 000-kernel/createModule.sh
+++ 000-kernel/createModule.sh
@@ -34,6 +34,7 @@ echo "Initial setup..."
 
 KERNELMAJORVERSION=${KERNELVERSION:0:1}
 KERNELMINORVERSION=$(echo ${KERNELVERSION} | cut -d. -f2)
+KERNELPATCHVERSION=$(echo ${KERNELVERSION} | cut -d. -f3)
 CRIPPLEDMODULENAME="06-crippled_sources-${KERNELVERSION}"
 
 rm -fr ${MODULEPATH} && mkdir -p ${MODULEPATH}
@@ -69,7 +70,7 @@ rm ${MODULEPATH}/kernel-headers.SlackBuild
 
 echo "Downloading AUFS..."
 git clone https://github.com/sfjro/aufs-standalone ${MODULEPATH}/aufs_sources > /dev/null 2>&1 || { echo "Fail to download AUFS."; exit 1; }
-git -C ${MODULEPATH}/aufs_sources checkout origin/aufs${KERNELMAJORVERSION}.${KERNELMINORVERSION} > /dev/null 2>&1 || git -C ${MODULEPATH}/aufs_sources checkout origin/aufs${KERNELMAJORVERSION}.x-rcN > /dev/null 2>&1 || { echo "Fail to download AUFS for this kernel version."; exit 1; }
+git -C ${MODULEPATH}/aufs_sources checkout origin/aufs${KERNELMAJORVERSION}.${KERNELMINORVERSION}.${KERNELPATCHVERSION} > /dev/null 2>&1 || git -C ${MODULEPATH}/aufs_sources checkout origin/aufs${KERNELMAJORVERSION}.${KERNELMINORVERSION} > /dev/null 2>&1 || git -C ${MODULEPATH}/aufs_sources checkout origin/aufs${KERNELMAJORVERSION}.x-rcN > /dev/null 2>&1 || { echo "Fail to download AUFS for this kernel version."; exit 1; }
 
 cd $MODULEPATH/linux-${KERNELVERSION}
 
