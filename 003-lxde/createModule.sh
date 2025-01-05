c64aced5c9c052e9d9b027984c36fc20d5be8508
porteux <porteuxlinux@gmail.com>
2025-01-05 19:11:29 +0100

    .Fixed wrong version of adwaita-qt in LXQt .Fixed pavucontrol shortcut appearing in LXDE settings

--- 003-lxde/createModule.sh
+++ 003-lxde/createModule.sh
@@ -535,6 +535,7 @@ InstallAdditionalPackages
 
 sed -i "s|Core;|Utility;|g" $MODULEPATH/packages/usr/share/applications/gpicview.desktop
 sed -i "s|image/x-xpixmap|image/x-xpixmap;image/heic;image/jxl|g" $MODULEPATH/packages/usr/share/applications/gpicview.desktop
+sed -i "s|;Settings;|;|g" $MODULEPATH/packages/usr/share/applications/pavucontrol.desktop
 sed -i "s|System;|Utility;|g" $MODULEPATH/packages/usr/share/applications/pcmanfm.desktop
 
 ### add lxde session
