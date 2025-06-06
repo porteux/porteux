9aa98b7f0e7fd0adb4819a80c2d03ebc268f3e37
porteux <porteuxlinux@gmail.com>
2025-06-06 14:19:24 +0200

    Fixed Etcher download

--- porteux-app-store/applications/etcher.sh
+++ porteux-app-store/applications/etcher.sh
@@ -3,9 +3,16 @@
 CURRENTPACKAGE=etcher
 FRIENDLYNAME=Etcher
 CATEGORY=Utility
-FULLVERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/balena-io/etcher/releases/latest | rev | cut -d / -f 1 | rev)
-VERSION="${FULLVERSION//[vV]}"
-APPLICATIONURL="https://github.com/balena-io/etcher/releases/latest/download/balenaEtcher-${VERSION}-x64.AppImage"
+versions=$(curl -s https://github.com/balena-io/${CURRENTPACKAGE}/tags/ | grep "/balena-io/${CURRENTPACKAGE}/releases/tag/" | grep -oP "(?<=/balena-io/${CURRENTPACKAGE}/releases/tag/)[^\"]+" | uniq | grep -v "alpha" | grep -v "beta" | grep -v "rc[0-9]")
+url="https://github.com/balena-io/etcher/releases/download/"
+for version in $versions; do
+	if wget --spider -q "$url/${version}/balenaEtcher-${version//[vV]}-x64.AppImage"; then
+		VERSION="${version//[vV]}"
+		break
+	fi
+done
+
+APPLICATIONURL="$url/${version}/balenaEtcher-${VERSION}-x64.AppImage"
 ACTIVATEMODULE=$([[ "$@" == *"--activate-module"* ]] && echo "--activate-module")
 
 RESULT=$(/opt/porteux-scripts/porteux-app-store/appimage-builder.sh "$CURRENTPACKAGE" "$FRIENDLYNAME" "$CATEGORY" "$APPLICATIONURL" "$VERSION" "$ACTIVATEMODULE")
