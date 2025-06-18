dc6c5fdb1f6b93c8513fae42ae88a4de32688b19
porteux <porteuxlinux@gmail.com>
2025-06-18 22:56:19 +0200

    More improvements to build flags

--- builder-utils/setflags.sh
+++ builder-utils/setflags.sh
@@ -5,8 +5,8 @@ SetFlags() {
 
 	export KERNELVERSION="6.15.2"
 	export ARCHITECTURELEVEL="x86-64-v2"
-	export GCCFLAGS="-O3 -march=$ARCHITECTURELEVEL -mtune=generic -fno-semantic-interposition -fno-trapping-math -ftree-vectorize -fno-ident -fno-unwind-tables -fno-asynchronous-unwind-tables -ffunction-sections -fdata-sections -s -fmodulo-sched -floop-parallelize-all -fuse-linker-plugin -Wl,--as-needed -Wl,--gc-sections -Wl,-O1 -Wl,-sort-common -Wl,--build-id=none"
-	export CLANGFLAGS="-O3 -march=$ARCHITECTURELEVEL -mtune=generic -fno-semantic-interposition -fno-trapping-math -ftree-vectorize -fno-ident -fno-unwind-tables -fno-asynchronous-unwind-tables -ffunction-sections -fdata-sections"
+	export GCCFLAGS="-O3 -march=$ARCHITECTURELEVEL -mtune=generic -fno-semantic-interposition -fno-trapping-math -ftree-vectorize -fno-unwind-tables -fno-asynchronous-unwind-tables -ffunction-sections -fdata-sections -fno-ident -s -fmodulo-sched -floop-parallelize-all -fuse-linker-plugin -Wl,--as-needed -Wl,--gc-sections -Wl,-O1 -Wl,-sort-common -Wl,--build-id=none"
+	export CLANGFLAGS="-O3 -march=$ARCHITECTURELEVEL -mtune=generic -fno-semantic-interposition -fno-trapping-math -ftree-vectorize -fno-unwind-tables -fno-asynchronous-unwind-tables -ffunction-sections -fdata-sections"
 	export RUSTFLAGS="-Copt-level=3 -Ctarget-cpu=$ARCHITECTURELEVEL -Ctarget-cpu=generic -Clto=fat -Zdylib-lto -Cpanic=abort -Cstrip=debuginfo -Cembed-bitcode=yes -Ccodegen-units=1 -Zlocation-detail=none"
 
 	systemFullVersion=$(cat /etc/slackware-version)
@@ -33,8 +33,8 @@ SetFlags() {
 		export SYSTEMBITS="64"
 	fi
 
-	#export SLACKWAREDOMAIN="http://ftp.slackware.com/pub"
-	export SLACKWAREDOMAIN="http://slackware.uk"
+	export SLACKWAREDOMAIN="http://ftp.slackware.com/pub"
+	#export SLACKWAREDOMAIN="http://slackware.uk"
 	export REPOSITORY="$SLACKWAREDOMAIN/slackware/slackware$SYSTEMBITS-$SLACKWAREVERSION/slackware$SYSTEMBITS"
 	export PATCHREPOSITORY="$SLACKWAREDOMAIN/slackware/slackware$SYSTEMBITS/patches"
 	export SOURCEREPOSITORY="$SLACKWAREDOMAIN/slackware/slackware$SYSTEMBITS-$SLACKWAREVERSION/source"
