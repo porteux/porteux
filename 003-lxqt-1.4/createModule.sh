39f9bc49e90478c04b9921f7ca4869b1aae47de9
porteux <porteuxlinux@gmail.com>
2025-08-07 11:30:05 +0200

    Another attempt to make xpdf download correctly in LXQt

--- 003-lxqt-1.4/createModule.sh
+++ 003-lxqt-1.4/createModule.sh
@@ -154,7 +154,13 @@ rm -fr $MODULEPATH/${currentPackage}
 
 currentPackage=xpdf
 mkdir $MODULEPATH/${currentPackage} && cd $MODULEPATH/${currentPackage}
-wget -r -nH --cut-dirs=6 --no-parent --reject="index.html*" ${SLACKWAREDOMAIN}/slackware/slackware64-current/source/xap/${currentPackage}/ || exit 1
+wget -r -nd --no-parent -l1 --reject="index.html*" ${SLACKWAREDOMAIN}/slackware/slackware64-current/source/xap/${currentPackage}
+mkdir lang && cd lang
+wget -r -nd --no-parent -l1 --reject="index.html*" ${SLACKWAREDOMAIN}/slackware/slackware64-current/source/xap/${currentPackage}/lang
+cd ..
+mkdir patches && cd patches
+wget -r -nd --no-parent -l1 --reject="index.html*" ${SLACKWAREDOMAIN}/slackware/slackware64-current/source/xap/${currentPackage}/patches
+cd $MODULEPATH/${currentPackage}
 sed -i "s|-O[23].*|$GCCFLAGS -ffat-lto-objects\"|g" ${currentPackage}.SlackBuild
 sed -i "s|-DXPDFWIDGET_PRINTING=1|-DMULTITHREADED=ON|g" ${currentPackage}.SlackBuild
 sed -z -i "s|mkdir build\n|sed -i \"s\|initialSidebarState = gTrue\|initialSidebarState = gFalse\|g\" xpdf/GlobalParams.cc\nmkdir build\n|g" ${currentPackage}.SlackBuild
