41bf8650a598cd35b1e9593871280ed90fff25f4
porteux <porteuxlinux@gmail.com>
2025-09-02 02:53:30 +0200

    Removed gtkmm4 patch because Slackware already fixed it

--- 003-gnome/createModule.sh
+++ 003-gnome/createModule.sh
@@ -115,9 +115,6 @@ done
 rm $MODULEPATH/packages/blueprint-compiler*
 rm $MODULEPATH/packages/gperf*
 
-# required to build gnome-system-monitor until gtkmm4 gets updated to 4.18.0+
-patch -p0 -d / < $SCRIPTPATH/deps/gtkmm4-iconpaintable.patch || exit 1
-
 # gnome packages
 for package in \
 	libadwaita \
