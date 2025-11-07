069e0d2fa5f32af9cf6c782f5c2892c038b7a60a
porteux <porteuxlinux@gmail.com>
2025-11-07 18:33:10 +0100

    .Fixed lld flag .Fixed 7zip build in stable .Fixed xfce4-screenshooter in stable

--- builder-utils/setflags.sh
+++ builder-utils/setflags.sh
@@ -6,10 +6,10 @@ SetFlags() {
 	export KERNELVERSION="6.17.7"
 	export ARCHITECTURELEVEL="x86-64-v2"
 	export GCCFLAGS="-O3 -march=$ARCHITECTURELEVEL -mtune=generic -fno-semantic-interposition -fno-trapping-math -ftree-vectorize -fno-unwind-tables -fno-asynchronous-unwind-tables -ffunction-sections -fdata-sections -flto=auto -fno-ident -fmodulo-sched -floop-parallelize-all -fuse-linker-plugin"
-	export LDFLAGS="-Wl,--gc-sections -Wl,--as-needed -Wl,--build-id=none -Wl,-O2 -Wl,--strip-all -Wl,--sort-section=alignment -Wl,-z,pack-relative-relocs -Wl,-sort-common"
+	export LDFLAGS="-Wl,--gc-sections -Wl,--as-needed -Wl,--build-id=none -Wl,-O2 -Wl,--strip-all -Wl,--sort-section=alignment -Wl,-sort-common"
 	export CLANGFLAGS="-O3 -march=$ARCHITECTURELEVEL -mtune=generic -fno-semantic-interposition -fno-trapping-math -ftree-vectorize -fno-unwind-tables -fno-asynchronous-unwind-tables -ffunction-sections -fdata-sections -flto=auto -Wno-unused-command-line-argument"
 	export LLDFLAGS="${LDFLAGS/ -Wl,-sort-common/} -fuse-ld=lld -Wl,--icf=safe -Wl,--lto-O3"
-	export RUSTFLAGS="-Copt-level=3 -Ctarget-cpu=$ARCHITECTURELEVEL -Ztune-cpu=generic -Clink-arg=-ffunction-sections -Clink-arg=-fdata-sections -Cforce-unwind-tables=no -Clto=thin -Clinker=clang -Clink-arg=-fuse-ld=lld -Clink-arg=-Wl,--gc-sections -Wl,--as-needed -Wl,--build-id=none -Clink-arg=-Wl,-O2 -Clink-arg=-Wl,--strip-all -Clink-arg=-Wl,--icf=safe -Cpanic=abort -Cdebuginfo=0 -Cembed-bitcode=yes -Cincremental=yes -Zdylib-lto -Zlocation-detail=none" # -Ccodegen-units=1
+	export RUSTFLAGS="-Copt-level=s -Ctarget-cpu=$ARCHITECTURELEVEL -Ztune-cpu=generic -Clink-arg=-ffunction-sections -Clink-arg=-fdata-sections -Cforce-unwind-tables=no -Clto=thin -Clinker=clang -Clink-arg=-fuse-ld=lld -Clink-arg=-Wl,--gc-sections -Wl,--as-needed -Wl,--build-id=none -Clink-arg=-Wl,-O2 -Clink-arg=-Wl,--strip-all -Clink-arg=-Wl,--icf=safe -Cpanic=abort -Cdebuginfo=0 -Cembed-bitcode=yes -Cincremental=yes -Zdylib-lto -Zlocation-detail=none" # -Ccodegen-units=1
 	export RUSTC_BOOTSTRAP=1 # allows -Z unstable flags on stable compiler
 
 	export PORTEUXVERSION=$(git -C "$PWD" branch --show-current)
