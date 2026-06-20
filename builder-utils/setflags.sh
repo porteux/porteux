25dd3cca768fc1e3027b113236f6aba90e065f08
porteux <porteuxlinux@gmail.com>
2026-06-20 20:30:20 +0200

    Bumped kernel to 7.1.1

--- builder-utils/setflags.sh
+++ builder-utils/setflags.sh
@@ -3,7 +3,7 @@
 SetFlags() {
 	MODULENAME="$1"
 
-	export KERNELVERSION="7.1"
+	export KERNELVERSION="7.1.1"
 	export ARCHITECTURELEVEL="x86-64-v2"
 	export GCC_CFLAGS="-O3 -march=$ARCHITECTURELEVEL -mtune=generic -fno-semantic-interposition -fno-trapping-math -fno-math-errno -ftree-vectorize -fno-unwind-tables -fno-asynchronous-unwind-tables -ffunction-sections -fdata-sections -flto=auto -fno-plt -fipa-pta -fno-ident -fmodulo-sched -fuse-linker-plugin -fgraphite-identity -floop-nest-optimize -fdevirtualize-at-ltrans -fipa-reorder-for-locality -fsched-pressure -fgcse-sm -fgcse-las"
 	export GCC_CXXFLAGS="$GCC_CFLAGS -fvisibility-inlines-hidden"
