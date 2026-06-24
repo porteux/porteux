835c1a02ee1b9a5f9b0b441e4a3339f019aa775c
porteux <porteuxlinux@gmail.com>
2026-06-24 18:30:44 +0200

    Added Brave Origin (thanks to matcarfer)

--- porteux-app-store/applications/brave.sh
+++ porteux-app-store/applications/brave.sh
@@ -13,14 +13,14 @@ fi
 if [ "$#" -lt 1 ]; then
 	echo "Usage: $0 [channel] [language] [optional: --activate-module]"
 	echo "If no language is specified, en-US will be set"
-	echo "Channels available: stable"
+	echo "Channels available: stable | origin"
 	echo ""
 	echo "Example: $0 stable pt-BR"
 	exit 1
 fi
 
 # Global variables
-APP="brave-browser"
+REPO="brave-browser"
 FRIENDLYPACKAGENAME="brave"
 CHANNEL=$([ "$1" ] && echo "$1" || echo "stable")
 LANGUAGE=$([ "$2" ] && echo "$2" || echo "en-US")
@@ -29,6 +29,10 @@ TARGET_DIR="$PORTDIR/modules"
 TMP="/tmp"
 WGET_WITH_TIME_OUT="wget -T 15"
 
+# Channel-dependent package name and install folder (origin and stable share the brave-browser repo/release)
+APP=$([ "$CHANNEL" == "origin" ] && echo "brave-origin" || echo "brave-browser")
+OPT_FOLDER=$([ "$CHANNEL" == "origin" ] && echo "brave-origin" || echo "brave")
+
 # Functions
 create_application_temp_dir() {
 	rm -fr "${TMP:?}/$1"
@@ -36,11 +40,13 @@ create_application_temp_dir() {
 }
 
 locale_striptease() {
-	find "$MODULEDIR/opt/brave.com/brave/locales" -mindepth 1 -maxdepth 1 \( -type f -o -type d \) ! \( -name "en-US.*" -o -name "en_US.*" -o -name "${LANGUAGE}.*" \) -delete
-	find "$MODULEDIR/opt/brave.com/brave/resources/brave_extension/_locales" -mindepth 1 -maxdepth 1 -type d ! \( -name "en" -o -name "${LANGUAGE//-/_}" \) -exec rm -fr {} +
+	find "$MODULEDIR/opt/brave.com/$OPT_FOLDER/locales" -mindepth 1 -maxdepth 1 \( -type f -o -type d \) ! \( -name "en-US.*" -o -name "en_US.*" -o -name "${LANGUAGE}.*" \) -delete
+	find "$MODULEDIR/opt/brave.com/$OPT_FOLDER/resources/brave_extension/_locales" -mindepth 1 -maxdepth 1 -type d ! \( -name "en" -o -name "${LANGUAGE//-/_}" \) -exec rm -fr {} +
 }
 
 striptease() {
+	rm -fr "$MODULEDIR/usr/share/appdata"
+	rm -fr "$MODULEDIR/usr/share/gnome-control-center"
 	rm -fr "$MODULEDIR/usr/share/man"
 	locale_striptease
 }
@@ -60,22 +66,25 @@ finisher() {
 }
 
 make_module_brave() {
-	if [ "$CHANNEL" != "stable" ]; then
-		echo "Non-existent channel. Options: stable" && exit 1
+	if [ "$CHANNEL" != "stable" ] && [ "$CHANNEL" != "origin" ]; then
+		echo "Non-existent channel. Options: stable | origin" && exit 1
 	fi
 
-	local FULLVERSION=$(curl -s https://api.github.com/repos/brave/${APP}/releases/latest | grep "\"tag_name\":" | cut -d \" -f 4 | head -n 1)
+	local FULLVERSION=$(curl -s https://api.github.com/repos/brave/${REPO}/releases/latest | grep "\"tag_name\":" | cut -d \" -f 4 | head -n 1)
 	local pkgver="${FULLVERSION//[vV]}"
 	local pkg_name=$(get_module_name "$CHANNEL" "$pkgver" "x86_64")
 	MODULEDIR="$TMP/$APP/$pkg_name"
 
 	create_application_temp_dir "$APP"
 
-	$WGET_WITH_TIME_OUT --content-disposition "https://github.com/brave/${APP}/releases/download/v${pkgver}/${APP}-${pkgver}-1.x86_64.rpm" -P "$TMP/$APP" || exit 1
+	$WGET_WITH_TIME_OUT --content-disposition "https://github.com/brave/${REPO}/releases/download/v${pkgver}/${APP}-${pkgver}-1.x86_64.rpm" -P "$TMP/$APP" || exit 1
 	mkdir -p "$MODULEDIR"
 	rpm2cpio "$TMP/$APP/${APP}-${pkgver}-1.x86_64.rpm" | cpio -idmv -D "$MODULEDIR" || exit 1
 	chmod 755 "$MODULEDIR"
 	sed -i "s|Exec=|Exec=env LANGUAGE=${LANGUAGE} |g" "$MODULEDIR/usr/share/applications/${APP}.desktop"
+
+	mkdir -p "$MODULEDIR/usr/share/icons/hicolor/256x256/apps"
+	cp "$MODULEDIR/opt/brave.com/$OPT_FOLDER/product_logo_256.png" "$MODULEDIR/usr/share/icons/hicolor/256x256/apps/${APP}.png"
 
 	finisher "$pkg_name"
 }
