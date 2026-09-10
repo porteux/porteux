876aeaa11d59d132901bf5c9a00f48d320fc5702
porteux <porteuxlinux@gmail.com>
2026-09-10 13:23:50 +0200

    Improved build flags

--- builder-utils/set-flags.sh
+++ builder-utils/set-flags.sh
@@ -5,12 +5,12 @@ set_flags() {
 
 	export KERNEL_VERSION="7.2.4"
 	export ARCHITECTURE_LEVEL="x86-64-v2"
-	export GCC_CFLAGS="-O3 -march=$ARCHITECTURE_LEVEL -mtune=generic -fno-semantic-interposition -fno-trapping-math -fno-unwind-tables -fno-asynchronous-unwind-tables -ffunction-sections -fdata-sections -flto=auto -fno-plt -fipa-pta -fno-ident -fmodulo-sched -fmodulo-sched-allow-regmoves -floop-nest-optimize -fdevirtualize-at-ltrans -fipa-reorder-for-locality -fgcse-sm -fgcse-las -favoid-store-forwarding"
+	export GCC_CFLAGS="-O3 -march=$ARCHITECTURE_LEVEL -mtune=generic -mtls-dialect=gnu2 -fno-semantic-interposition -fno-trapping-math -fno-unwind-tables -fno-asynchronous-unwind-tables -ffunction-sections -fdata-sections -flto=auto -fno-plt -fipa-pta -fno-ident -floop-nest-optimize -fdevirtualize-at-ltrans -fipa-reorder-for-locality -fgcse-sm -fgcse-las -favoid-store-forwarding"
 	export GCC_CXXFLAGS="$GCC_CFLAGS -fvisibility-inlines-hidden"
 	export LDFLAGS="-Wl,--gc-sections -Wl,--as-needed -Wl,--build-id=none -Wl,-O2 -Wl,--strip-all -Wl,--sort-common -Wl,--sort-section=alignment -Wl,-z,pack-relative-relocs -Wl,-z,noseparate-code -Wl,--hash-style=gnu"
-	export CLANG_CFLAGS="-O3 -march=$ARCHITECTURE_LEVEL -mtune=generic -fno-semantic-interposition -fno-trapping-math -fno-unwind-tables -fno-asynchronous-unwind-tables -ffunction-sections -fdata-sections -flto=auto -fno-plt -fno-ident -faddrsig -fveclib=libmvec -Wno-unused-command-line-argument -mllvm -enable-dfa-jump-thread"
+	export CLANG_CFLAGS="-O3 -march=$ARCHITECTURE_LEVEL -mtune=generic -mtls-dialect=gnu2 -fno-pie -fno-semantic-interposition -fno-trapping-math -fno-unwind-tables -fno-asynchronous-unwind-tables -ffunction-sections -fdata-sections -flto=auto -fno-plt -fno-ident -faddrsig -fveclib=libmvec -Wno-unused-command-line-argument -mllvm -enable-dfa-jump-thread"
 	export CLANG_CXXFLAGS="$CLANG_CFLAGS -fvisibility-inlines-hidden -fwhole-program-vtables -fstrict-vtable-pointers"
-	export LLDFLAGS="${LDFLAGS/-Wl,--sort-common/} -fuse-ld=lld -Wl,--icf=safe -Wl,--lto-O3 -Wl,--lto-CGO3 -Wl,--lto-whole-program-visibility"
+	export LLDFLAGS="${LDFLAGS/-Wl,--sort-common/} -fuse-ld=lld -Wl,--no-pie -Wl,-z,keep-text-section-prefix -Wl,--icf=safe -Wl,--lto-O3 -Wl,--lto-CGO3 -Wl,--lto-whole-program-visibility"
 	export RUSTFLAGS="-Copt-level=3 -Ctarget-cpu=$ARCHITECTURE_LEVEL -Ztune-cpu=generic -Cstrip=symbols -Cforce-unwind-tables=no -Clto=fat -Clinker=clang -Clink-arg=-fuse-ld=lld -Clink-arg=-Wl,--gc-sections -Clink-arg=-Wl,-O2 -Clink-arg=-Wl,--strip-all -Clink-arg=-Wl,--icf=safe -Clink-arg=-Wl,--lto-O3 -Clink-arg=-Wl,--lto-CGO3 -Clink-arg=-Wl,--lto-whole-program-visibility -Clink-arg=-Wl,-z,pack-relative-relocs -Clink-arg=-Wl,--hash-style=gnu -Cllvm-args=-enable-dfa-jump-thread -Cpanic=unwind -Cdebuginfo=0 -Cembed-bitcode=yes -Zdylib-lto -Zlocation-detail=none -Zfmt-debug=shallow -Ccodegen-units=1"
 	export RUSTC_BOOTSTRAP=1 # allows -Z unstable flags on stable compiler
 	
@@ -48,8 +48,8 @@ set_flags() {
 		export SYSTEM_BITS="64"
 	fi
 
-	#export SLACKWARE_DOMAIN="https://mirrors.slackware.com"
+	export SLACKWARE_DOMAIN="https://mirrors.slackware.com"
 	#export SLACKWARE_DOMAIN="https://slackware.uk"
-	export SLACKWARE_DOMAIN="http://ftp.slackware.com/pub"
+	#export SLACKWARE_DOMAIN="http://ftp.slackware.com/pub"
 	export REPOSITORY="$SLACKWARE_DOMAIN/slackware/slackware$SYSTEM_BITS-$SLACKWARE_VERSION/slackware$SYSTEM_BITS"
 }
